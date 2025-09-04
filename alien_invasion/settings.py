37218ad17190aa62005997351cc48159778ebf07
CN <1243763012@qq.com>
2025-09-04 18:02:09 +0800

    小游戏子弹和飞机前后左右飞行

--- alien_invasion/settings.py
+++ alien_invasion/settings.py
@@ -5,3 +5,10 @@ class Settings():
         self.screen_width = 900
         self.screen_height = 600
         self.bg_color = (230,230,230)
+        self.ship_speed_factor = 1.5
+        self.ship_forwardspeed_factor = 0.5
+        #子弹设置
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color =60,60,60
