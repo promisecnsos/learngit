3df7d3d6cb13b62c33c93273adb31118c573e5ac
CN <1243763012@qq.com>
2025-09-05 17:58:53 +0800

    生成子弹，创建外星人

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -2,6 +2,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 def run_game():
     #初始化pygame、设置和屏幕对象
@@ -12,14 +13,19 @@ def run_game():
     )
     pygame.display.set_caption("Alien Invasion")
 
-    #创建一艘飞船
+    #创建一艘飞船、一个子弹编组和一个外星人编组
     ship = Ship(ai_settings,screen)
+    bullets = Group()
+    aliens = Group()
 
-    
+    #创建外星人群
+    gf.create_fleet(ai_settings,screen,aliens)
 
     #开始游戏主循环
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings,screen,ship,bullets)
         ship.update()
-        gf.update_screen(ai_settings,screen,ship)
+        gf.update_bullets(bullets)
+            
+        gf.update_screen(ai_settings,screen,ship,aliens,bullets)
 run_game()
--- alien_invasion/bullet.py
+++ alien_invasion/bullet.py
@@ -6,7 +6,7 @@ class Bullet(Sprite):
 
     def __init__(self, ai_settings,screen,ship):
         """在飞船所在位置创建一个子弹对象"""
-        super(Bullet).__init__()
+        super(Bullet,self).__init__()
         self.screen = screen
 
         #在（0,0）处创建一个表示子弹的矩形，再设置正确的位置
@@ -18,3 +18,15 @@ class Bullet(Sprite):
         self.y =float(self.rect.y)
         self.color = ai_settings.bullet_color
         self.speed_factor = ai_settings.bullet_speed_factor
+
+    def update(self):
+        """向上移动子弹"""
+
+        #更新表示子弹位置的小数值
+        self.y -= self.speed_factor
+        #更新表示子弹的rect的位置
+        self.rect.y = self.y
+
+    def draw_bullet(self):
+        """在屏幕上绘制子弹"""
+        pygame.draw.rect(self.screen,self.color,self.rect)
